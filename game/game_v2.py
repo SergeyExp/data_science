2aaf39c9d7293dae61c83bdfef11eb58d7805a33
SergeyExp <zoneactive@gmail.com>
2022-10-05 18:22:32 +0500

    optimized_algorithm

--- game/game_v2.py
+++ game/game_v2.py
@@ -14,22 +14,33 @@ def random_predict(number: int = 1) -> int:
     Returns:
         int: Число попыток
     """
-    count = 0
+    
+    number = np.random.randint(1, 101)  # число рандомно загадали
+    #print(f'загадали: {number}')
 
+    count = 0
+    i = 0
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
+        if 50 > number >= 1 :# последовательный перебор в диапазоне 1-50 до загаданного числа
+            seach_number = 1 + i 
+            i += 1 
+            if seach_number == number:
+                #print(f'отгадали: {number}, попыток: {count}')
+                break  # выход из цикла если угадали
+        else:# последовательный перебор в диапазоне 50-100 до загаданного числа
+            seach_number = 50 + i 
+            i += 1 
+            if seach_number == number:
+                #print(f'отгадали: {number}, попыток: {count}')
+                break  # выход из цикла если угадали
     return count
-
+    
 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
-
     Args:
         random_predict ([type]): функция угадывания
-
     Returns:
         int: среднее количество попыток
     """
@@ -37,8 +48,8 @@ def score_game(random_predict) -> int:
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
-    for number in random_array:
-        count_ls.append(random_predict(number))
+    for seach_number in random_array:
+        count_ls.append(random_predict(seach_number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
--- project_0/test.py
+++ project_0/test.py
@@ -1,8 +1,17 @@
-print('123333')
-import numpy as np
-print('super')
-print('daska')
-print('git')
-n = int(input())
-if n > 0: print(f'big bang {n}')
-print('rt')
+    if predict_number >= 50:
+        number = np.random.randint(50, 101)
+        while True:
+            count += 1
+            if number == predict_number:
+                print(f'отгадали: {number}, попыток: {count}')
+                break  # выход из цикла если угадали
+        return count 
+    else:
+        number = np.random.randint(1, 50)   
+        while True:
+            count += 1
+            if number == predict_number:
+                print(f'отгадали: {number}, попыток: {count}')
+                break  # выход из цикла если угадали
+        #print(f'отгадали: {number}') 
+        return count    
